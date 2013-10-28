673044ffe69cc2ac770d50148387de04b88e4439
Andrey Povelichenko <andrey.povelichenko@quickblox.com>
2013-10-28 16:39:56 +0200

    change a test room

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 //QB Account params
 var QBPARAMS = {
-	app_id      : 3907,
-	auth_key    : 'jRVze-6OzVDh-WX',
-	auth_secret : 'uX8dZDexGW8TrEe'
+	app_id      : 4753,
+	auth_key    : 'LPC25Ms9CFvz9sQ',
+	auth_secret : 'ePYd72WGZdZgawq'
 }
 
 //Chat params
 var CHAT = {
 	server      : 'chat.quickblox.com',
 	bosh_url    : 'http://chat.quickblox.com:5280',
-	roomJID     : '3907_test@muc.chat.quickblox.com'
+	roomJID     : '4753_test@muc.chat.quickblox.com'
 }
