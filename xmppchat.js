b700c052cd494acccf591efd9bf3ce9a7a0c942c
Andrey Povelichenko <andrey.povelichenko@quickblox.com>
2013-11-08 10:29:05 +0200

    small fix height

--- xmppchat.js
+++ xmppchat.js
@@ -430,7 +430,7 @@ function widgetView() {
 	}
 	
 	if (WIDGET_WIDTH < 350 || WIDGET_HEIGHT < 350) {
-		$('#chat .chat-content').css('height', WIDGET_HEIGHT - 79);
+		$('#chat .chat-content').css('height', WIDGET_HEIGHT - 80);
 		$('#chat .chat-footer textarea').css('width', WIDGET_WIDTH - 100);
 		$('#area').css('width', WIDGET_WIDTH - 86);
 	}
