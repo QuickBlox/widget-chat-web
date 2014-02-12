7814a6f0855cc341d9aaee1f7ab912a69a2f2288
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-02-12 03:45:32 +0200

    global changes in naming values

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 /* Insert your QB application credentials
 -----------------------------------------------*/
 var QBAPP = {
-	app_id: 3907,
-	auth_key: 'jRVze-6OzVDh-WX',
-	auth_secret: 'uX8dZDexGW8TrEe'
+	appID: 6566,
+	authKey: 'pm6CarZjJQC9c3b',
+	authSecret: 'pFPBSJD8t2xY5Br'
 };
 var CHAT = {
-	room_jid: '3907_demo_room@muc.chat.quickblox.com',
+	roomJID: '6566_test@muc.chat.quickblox.com',
 	server: 'chat.quickblox.com',
-	bosh_server: 'http://chat.quickblox.com:5280'
+	bosh: 'http://chat.quickblox.com:5280'
 };
 var FBAPP_ID = '368137086642884';
