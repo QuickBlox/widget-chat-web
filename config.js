890ec677fc6ed889409d9182a6b4d90c4c7c73bb
AndreyPovelichenko <andrey.povelichenko@quickblox.com>
2014-02-19 15:45:37 +0200

    release v2.2.0

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 /* Insert your QB application credentials
 -----------------------------------------------*/
 var QBAPP = {
-	appID: 6566,
-	authKey: 'pm6CarZjJQC9c3b',
-	authSecret: 'pFPBSJD8t2xY5Br'
+	appID: 3907,
+	authKey: 'jRVze-6OzVDh-WX',
+	authSecret: 'uX8dZDexGW8TrEe'
 };
 var CHAT = {
-	roomJID: '6566_test@muc.chat.quickblox.com',
+	roomJID: '3907_demo_room@muc.chat.quickblox.com',
 	server: 'chat.quickblox.com',
 	bosh: 'http://chat.quickblox.com:5280'
 };
